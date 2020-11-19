4f370225300c539c914ee923328bf6739167ad78
Victor Tavares <victor.as156@gmail.com>
2020-11-19 16:44:04 -0300

    Projeto criado

---
+++ blogPessoalDeploy-completo/blogpessoal-api/src/main/java/generation/org/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package generation.org.blogPessoal.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import generation.org.blogPessoal.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+		public Optional<Usuario> findByUsuario(String usuario);
+}
